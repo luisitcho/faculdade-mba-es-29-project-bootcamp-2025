148aafd6dbd0fffe3012eeba5a3fc3254f334181
luisitcho <luishenrisc1@gmail.com>
2025-10-14 20:28:08 -0300

    Project Bootcamp - layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,9 +5,9 @@ import { Analytics } from '@vercel/analytics/next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.app',
+  title: 'Controle de Estoque',
+  description: 'Sistema de controle de estoque',
+  generator: 'Controle de Estoque',
 }
 
 export default function RootLayout({
